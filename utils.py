18a01b125215d764cf0c4aaf74f1e4b618b2b4c2
glendon.mo <glendon.mo@pixel-nexus.com>
2024-03-25 23:15:59 +0100

    feat(initial commit): basic working prototype

---
+++ utils.py
@@ -0,0 +1,11 @@
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
