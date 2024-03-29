6e13fe3b1996b1432734ffb104ec3c4bbf67b428
glendon.mo <glendon.mo@pixel-nexus.com>
2024-03-29 22:22:05 +0100

    feat: helper functions to convert shortcut display names into Qt.MouseButton and Qt.Key

--- ui/config/__init__.py
+++ ui/config/__init__.py
@@ -3,6 +3,9 @@ from .kis_shortcut_configuration import (
     buttons_to_text,
     buttons_input_to_text,
     ShortcutType,
+    text_to_keys,
+    text_to_buttons,
+    text_input_to_buttons,
 )
 
 __all__ = [
@@ -10,4 +13,7 @@ __all__ = [
     "buttons_to_text",
     "buttons_input_to_text",
     "ShortcutType",
+    "text_to_keys",
+    "text_to_buttons",
+    "text_input_to_buttons",
 ]
--- ui/config/kis_shortcut_configuration.py
+++ ui/config/kis_shortcut_configuration.py
@@ -63,7 +63,9 @@ def keys_to_text(keys: list[Qt.Key]) -> str:
     for key in keys:
         if len(text) > 0:
             text += SEPARATOR
-        default_key = QtGui.QKeySequence(key).toString(QtGui.QKeySequence.NativeText)
+        default_key = QtGui.QKeySequence(key).toString(
+            QtGui.QKeySequence.NativeText
+        )
         key_name = KEY_NAME_MAP.get(key, default_key)
         text += key_name
     if len(text) == 0:
@@ -80,3 +82,49 @@ def buttons_input_to_text(keys, buttons):
         keys_text = keys_to_text(keys)
         return button_text + "+" + keys_text
     return button_text
+
+
+def text_to_buttons(text, separator=SEPARATOR) -> list[Qt.MouseButton]:
+    """Map a mouse button shortcut to a list of Qt.Mousebutton.
+    Expects each button to be separated by given separator.
+    """
+    buttons = []
+    for button in text.split(separator):
+        for qt_button in BUTTON_NAME_MAP:
+            if button == BUTTON_NAME_MAP[qt_button]:
+                buttons.append(qt_button)
+    return buttons
+
+
+def text_to_keys(text, separator=SEPARATOR) -> list[Qt.Key]:
+    """Map a keyboard shortcut to a list of Qt.Key.
+    Expects each key to be separated by given separator.
+    """
+    keys = []
+    # TODO(glendon):
+    # Right now, the Left and Right mouse button names
+    # will be interpreted as the Left and Right keys.
+    # There should be a better way of handling this.
+    buttons = [BUTTON_NAME_MAP[button] for button in BUTTON_NAME_MAP]
+
+    for key in text.split(separator):
+        # ignoring keys that have the same name as the mouse buttons
+        if key in buttons:
+            continue
+        qt_key = [
+            qt_key for qt_key in KEY_NAME_MAP if KEY_NAME_MAP[qt_key] == key
+        ]
+        if not qt_key:
+            key_sequence = QtGui.QKeySequence(key)
+            qt_key = [Qt.Key(key_sequence[0])]
+        keys += qt_key
+    return keys
+
+
+def text_input_to_buttons(text, separator=SEPARATOR):
+    """Map mouse buttons and keys to lists of Qt.Key and Qt.MouseButton.
+    Expects each button and key to be separated by given separator.
+    """
+    keys = text_to_keys(text, separator)
+    buttons = text_to_buttons(text, separator)
+    return buttons, keys
